a90202e829a7df2ed05bd5cc61b5caae6436b329
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-01-22 15:52:52 -0300

    Preciso concluir uma funcao

--- smart-contracts/contracts/Eleicao.sol
+++ smart-contracts/contracts/Eleicao.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.20;
 
 contract Eleicao {
+    /**
+     * @dev Informações de um candidato
+     */
     struct Candidato {
         string nome;
         string partido;
@@ -9,18 +12,32 @@ contract Eleicao {
         uint256 quantidadeDeVotos;
         uint16 numeroDeVotacao;
     }
-    mapping(uint16 numeroDeVotacao => Candidato informacoes) public candidatoPorNumero;
+    mapping(uint16 numeroDeVotacao => Candidato informacoes)
+        public candidatoPorNumero;
     uint16[] public listaDeNumerosCadastrados;
-    constructor(Candidato[] memory candidatosIniciais){
-        _cadastrarCandidatosIniciais(candidatosIniciais);
+
+    constructor(Candidato[] memory candidatosIniciais) {
+        _cadastrarCandidatos(candidatosIniciais);
     }
 
-
-    function _cadastrarCandidatosIniciais(Candidato[] memory candidatosIniciais) private{
-        for (uint i = 0; i < candidatosIniciais.length; i++) {
-            uint16 numeroDoCandidato =candidatosIniciais[i].numeroDeVotacao; 
-            candidatoPorNumero[numeroDoCandidato] = candidatosIniciais[i];
+    /**
+     *
+     * @param candidatos Candidatos a serem cadastrados;
+     */
+    function _cadastrarCandidatos(Candidato[] memory candidatos) private {
+        for (uint i = 0; i < candidatos.length; i++) {
+            uint16 numeroDoCandidato = candidatos[i].numeroDeVotacao;
+            candidatoPorNumero[numeroDoCandidato] = candidatos[i];
             listaDeNumerosCadastrados.push(numeroDoCandidato);
         }
+    }
+    function getCandidatos(uint256 indiceDePartida,uint256 indiceDeChegada) public view returns(Candidato[] memory){
+        //to do: concluir essa funcao
+        uint256 tamanhoDaPagina = (indiceDeChegada - indiceDePartida)+ 1;
+        Candidato[] memory candidatos = new Candidato[](tamanhoDaPagina);
+        for (uint256 i = indiceDePartida; i < tamanhoDaPagina; i++) {
+            candidatos[i] = candidatoPorNumero[listaDeNumerosCadastrados[i]];
+        }
+        return candidatos;
     }
 }
