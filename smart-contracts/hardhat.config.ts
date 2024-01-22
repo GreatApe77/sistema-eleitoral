5c3fa80a3807b8d45d0319f2cc835191957f9afa
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-01-22 15:09:29 -0300

    instala libs

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+import "solidity-docgen";
+import dotenv from "dotenv"
+dotenv.config()
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+	solidity: { compilers: [{ version: "0.8.20" }] },
+  networks:{
+    fantomTestnet:{
+      url:`${process.env.FANTOM_TESTNET_RPC_URL || "https://rpc.ankr.com/fantom_testnet"}`,
+      chainId:4002,
+      accounts:{
+        mnemonic:`${process.env.MNEMONIC}`
+      }
+    },
+    hardhatNode:{
+      url:"http://localhost:8545"
+    }
+  },
+  etherscan:{
+    apiKey:`${process.env.ETHERSCAN_API_KEY}`
+  }
 };
 
 export default config;
