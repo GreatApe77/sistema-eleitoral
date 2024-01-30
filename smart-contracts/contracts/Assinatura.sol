831511f8004eb5f1ddc373740b160943731b28d5
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-01-30 16:39:51 -0300

    assinatura

---
+++ smart-contracts/contracts/Assinatura.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import 
+
+contract Assinatura{
+    bytes32 private constant VOTACAO_TYPEHASH = keccak256("Votacao(uint16 numeroDoCandidato,uint256 nonce,uint256 prazo,bool votoComputado)");
+}
