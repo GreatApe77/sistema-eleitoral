da4919e088a92ccc866a70b923ddbdf7cfb2be55
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-02-04 21:53:37 -0300

    cria o repository

--- server/src/repositories/IEleitorRepository.ts
+++ server/src/repositories/IEleitorRepository.ts
@@ -1,5 +1,8 @@
 import Eleitor from "../models/Eleitor"
 export interface IEleitorRepository{
     findByCpf(cpf:string):Promise<Eleitor>
+    findByChavePublica(chavePublica:string):Promise<Eleitor>
     save(eleitor:Eleitor):Promise<void>
+    update(eleitor:Eleitor):Promise<void>
+    delete(cpf:string):Promise<void>
 }
--- server/src/repositories/implementations/EleitorRepositoryMongoDB.ts
+++ server/src/repositories/implementations/EleitorRepositoryMongoDB.ts
@@ -1,12 +1,34 @@
+import connectDB from "../../database/connect";
 import Eleitor from "../../models/Eleitor";
 import { IEleitorRepository } from "../IEleitorRepository";
-
+const COLLECTION_NAME = "eleitores"
 export default class EleitorRepositoryMongoDB implements IEleitorRepository{
-    findByCpf(cpf: string): Promise<Eleitor> {
+    async findByChavePublica(chavePublica: string): Promise<Eleitor> {
+        const db = await connectDB()
+        const eleitor = await db.collection(COLLECTION_NAME).findOne({chavePublica}) 
+        if(!eleitor){
+            throw new Error('Eleitor not found')
+        }
+        return new Eleitor({chavePublica:eleitor.chavePublica,cpf:eleitor.cpf},eleitor.id)
+    }
+    update(eleitor: Eleitor): Promise<void> {
         throw new Error("Method not implemented.");
     }
-    save(eleitor: Eleitor): Promise<void> {
+    delete(cpf: string): Promise<void> {
         throw new Error("Method not implemented.");
+    }
+    async findByCpf(cpf: string): Promise<Eleitor> {
+        const db = await connectDB()
+        const eleitor = await db.collection(COLLECTION_NAME).findOne({cpf}) 
+        if(!eleitor){
+            throw new Error('Eleitor not found')
+        }
+        return new Eleitor({chavePublica:eleitor.chavePublica,cpf:eleitor.cpf},eleitor.id)
+    }
+    async save(eleitor: Eleitor): Promise<void> {
+        const db = await connectDB()
+        await db.collection(COLLECTION_NAME).insertOne(eleitor,{forceServerObjectId:false})
+
     }
     
 }
