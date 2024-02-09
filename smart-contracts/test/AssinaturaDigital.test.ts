836f994b4f4ab8e16f281ef87b6abc128d19273a
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-02-09 13:51:48 -0300

    adiciona testes para AssinaturaDigital

--- smart-contracts/test/AssinaturaDigital.test.ts
+++ smart-contracts/test/AssinaturaDigital.test.ts
@@ -5,6 +5,7 @@ import {
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers, network } from "hardhat";
+import { VotacaoType } from "./utils/VotacaoType";
 const ANO_DA_ELEICAO = 2022;
 describe("AssinaturaDigital", () => {
   async function deployFixture() {
@@ -33,15 +34,7 @@ describe("AssinaturaDigital", () => {
       chainId: network.chainId,
       verifyingContract: await assinaturaDigital.getAddress(),
     };
-    const VotacaoType = {
-      Votacao: [
-        { name: "assinante", type: "address" },
-        { name: "numeroDoCandidato", type: "uint16" },
-        {name:"anoDaEleicao",type:"uint256"},
-        { name: "nonce", type: "uint256" },
-        { name: "prazo", type: "uint256" },
-      ],
-    };
+    
     const cincoMinutosNoFuturo = Math.floor(Date.now() / 1000 + 300);
     const values = {
       assinante: assinante.address,
@@ -90,15 +83,7 @@ describe("AssinaturaDigital", () => {
       chainId: network.chainId,
       verifyingContract: await assinaturaDigital.getAddress(),
     };
-    const VotacaoType = {
-      Votacao: [
-        { name: "assinante", type: "address" },
-        { name: "numeroDoCandidato", type: "uint16" },
-        {name:"anoDaEleicao",type:"uint256"},
-        { name: "nonce", type: "uint256" },
-        { name: "prazo", type: "uint256" },
-      ],
-    };
+    
     const cincoMinutosNoFuturo = Math.floor(Date.now() / 1000 + 300);
     const values = {
       assinante: assinante.address,
@@ -115,7 +100,7 @@ describe("AssinaturaDigital", () => {
     const assinaturaComponentizada = ethers.Signature.from(assinatura);
     const { v, r, s } = assinaturaComponentizada;
 
-    await time.increase(301);
+    await time.increase(1000);
     await expect(assinaturaDigital.assinar(
       assinante.address,
       values.numeroDoCandidato,
@@ -126,4 +111,45 @@ describe("AssinaturaDigital", () => {
       s
     )).to.be.revertedWithCustomError(assinaturaDigital,"AssinaturaDigital__ForaDoPrazo");
   });
+  it("Nao deve assinar se o contrato nao validar que o assinante da mensagem foi passado por parâmetro", async () => {
+    const { assinaturaDigital, signers, network } = await loadFixture(
+      deployFixture
+    );
+    const assinante = signers[1];
+    const domain = {
+      name: "Assinatura Digital",
+      version: "1",
+      chainId: network.chainId,
+      verifyingContract: await assinaturaDigital.getAddress(),
+    };
+    
+    const cincoMinutosNoFuturo = Math.floor(Date.now() / 1000 + 300);
+    const values = {
+      assinante: assinante.address,
+      numeroDoCandidato: 30,
+      anoDaEleicao:ANO_DA_ELEICAO,
+      nonce: await assinaturaDigital.nonces(assinante.address),
+      prazo: cincoMinutosNoFuturo,
+    };
+    const assinatura = await assinante.signTypedData(
+      domain,
+      VotacaoType,
+      values
+    );
+    const assinaturaComponentizada = ethers.Signature.from(assinatura);
+    const { v, r, s } = assinaturaComponentizada;
+    await expect(
+      assinaturaDigital.assinar(
+        signers[2].address,
+        values.numeroDoCandidato,
+        ANO_DA_ELEICAO,
+        cincoMinutosNoFuturo,
+        v,
+        r,
+        s
+      )
+    ).to.be.revertedWithCustomError(assinaturaDigital,"AssinaturaDigital__AssinanteInvalido");
+    
+    
+  })
 });
---
+++ smart-contracts/test/utils/VotacaoType.ts
@@ -0,0 +1,9 @@
+export const VotacaoType = {
+    Votacao: [
+      { name: "assinante", type: "address" },
+      { name: "numeroDoCandidato", type: "uint16" },
+      {name:"anoDaEleicao",type:"uint256"},
+      { name: "nonce", type: "uint256" },
+      { name: "prazo", type: "uint256" },
+    ],
+  };
