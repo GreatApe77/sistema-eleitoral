30c4d7fdeb9386e588dc3842b432f35c6072c4d5
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-02-11 14:35:52 -0300

    escrever testes de SistemaEleitoral

--- smart-contracts/test/SistemaEleitoral.test.ts
+++ smart-contracts/test/SistemaEleitoral.test.ts
@@ -11,10 +11,12 @@ import { deploySistemaEleitoralComEleicaoFixture } from "./utils/fixtures";
 
 
 describe("SistemaEleitoral",()=>{
-    it("Deve ser possivel criar uma eleição",async()=>{
+    it("Deve ser possivel anexar uma eleição",async()=>{
         const {sistemaEleitoral,eleicao} = await loadFixture(deploySistemaEleitoralComEleicaoFixture)
         expect(await sistemaEleitoral.anexarEleicao(await eleicao.getAnoDeEleicao(),eleicao.target)).to.emit(sistemaEleitoral,"EleicaoCriada")
     })
-    
+    it("Não Deve ser possível anexar uma eleição(Não é o administrador)",async()=>{
+      
+    })
         
 })
