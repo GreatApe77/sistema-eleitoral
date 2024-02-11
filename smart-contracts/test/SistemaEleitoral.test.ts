db030453324e259ef4c72d8054c110e07afb88c9
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-02-11 15:58:59 -0300

    adiciona testes de iniciar e encerrar eleiçoes

--- smart-contracts/test/SistemaEleitoral.test.ts
+++ smart-contracts/test/SistemaEleitoral.test.ts
@@ -17,21 +17,53 @@ describe("SistemaEleitoral",()=>{
     })
     it("Não Deve ser possível anexar uma eleição(Não é o administrador)",async()=>{
       const {sistemaEleitoral,eleicao,signers} = await loadFixture(deploySistemaEleitoralComEleicaoFixture)
-      expect(sistemaEleitoral.connect(signers[1]).anexarEleicao(await eleicao.getAnoDeEleicao(),eleicao.target)).to.be.revertedWithCustomError(sistemaEleitoral,"OwnableUnauthorizedAccount")
+      await expect(sistemaEleitoral.connect(signers[1]).anexarEleicao(await eleicao.getAnoDeEleicao(),eleicao.target)).to.be.revertedWithCustomError(sistemaEleitoral,"OwnableUnauthorizedAccount")
     })
     it("Nao deve ser posivel anexar uma eleição com o mesmo ano",async()=>{
       const {sistemaEleitoral,eleicao} = await loadFixture(deploySistemaEleitoralComEleicaoFixture)
       await sistemaEleitoral.anexarEleicao(await eleicao.getAnoDeEleicao(),eleicao.target)
-      expect(sistemaEleitoral.anexarEleicao(await eleicao.getAnoDeEleicao(),eleicao.target)).to.be.revertedWithCustomError(sistemaEleitoral,"SistemaEleitoral__EleicaoJaExiste")
+      await expect(sistemaEleitoral.anexarEleicao(await eleicao.getAnoDeEleicao(),eleicao.target)).to.be.revertedWithCustomError(sistemaEleitoral,"SistemaEleitoral__EleicaoJaExiste")
     })
     it("Não deve ser possível anexar uma eleição que não implemente a interface IEleicao",async()=>{
       const {sistemaEleitoral,signers} = await loadFixture(deploySistemaEleitoralComEleicaoFixture)
       const mockContract = await ethers.getContractFactory("MockContract")
       const mock = await mockContract.deploy()
-      expect(sistemaEleitoral.anexarEleicao(2024,mock.target)).to.be.revertedWithCustomError(sistemaEleitoral,"SistemaEleitoral__EleicaoNaoExiste")
+      await expect(sistemaEleitoral.anexarEleicao(2024,mock.target)).to.be.revertedWithCustomError(sistemaEleitoral,"SistemaEleitoral__EleicaoNaoExiste")
     })
     it("Não deve ser possível anexar uma eleição que seu número difere do ano passado ao parametro",async()=>{
       const {sistemaEleitoral,eleicao} = await loadFixture(deploySistemaEleitoralComEleicaoFixture)
-      expect(sistemaEleitoral.anexarEleicao(2025,eleicao.target)).to.be.revertedWithCustomError(sistemaEleitoral,"SistemaEleitoral__EleicaoNaoExiste")
+      await expect(sistemaEleitoral.anexarEleicao(2025,eleicao.target)).to.be.revertedWithCustomError(sistemaEleitoral,"SistemaEleitoral__EleicaoNaoExiste")
+    })
+    it("Deve iniciar uma eleição",async()=>{
+      const {sistemaEleitoral,eleicao} = await loadFixture(deploySistemaEleitoralComEleicaoFixture)
+      await sistemaEleitoral.anexarEleicao(await eleicao.getAnoDeEleicao(),eleicao.target)
+      expect(await sistemaEleitoral.iniciarEleicao(await eleicao.getAnoDeEleicao())).to.emit(eleicao,eleicao.getEvent("EleicaoIniciada").name)
+    })
+    it("Nao deve iniciar uma eleição (Não é o administrador)",async()=>{
+      const {sistemaEleitoral,eleicao,signers} = await loadFixture(deploySistemaEleitoralComEleicaoFixture)
+
+      await expect(sistemaEleitoral.connect(signers[1]).iniciarEleicao(await eleicao.getAnoDeEleicao())).to.be.revertedWithCustomError(sistemaEleitoral,"OwnableUnauthorizedAccount")
+    })
+    it("Nao deve iniciar uma eleição que não existe",async()=>{
+      const {sistemaEleitoral} = await loadFixture(deploySistemaEleitoralComEleicaoFixture)
+      await expect(sistemaEleitoral.iniciarEleicao(2024)).to.be.revertedWithCustomError(sistemaEleitoral,"SistemaEleitoral__EleicaoNaoExiste")
+    })
+    it("Deve encerrar uma eleição",async()=>{
+      const {sistemaEleitoral,eleicao} = await loadFixture(deploySistemaEleitoralComEleicaoFixture)
+      await sistemaEleitoral.anexarEleicao(await eleicao.getAnoDeEleicao(),eleicao.target)
+      await sistemaEleitoral.iniciarEleicao(await eleicao.getAnoDeEleicao())
+      await time.increase(60*60*24*30)
+      expect(await sistemaEleitoral.encerrarEleicao(await eleicao.getAnoDeEleicao())).to.emit(eleicao,eleicao.getEvent("EleicaoEncerrada").name)
+    })
+    it("Nao deve encerrar uma eleição (Não é o administrador)",async()=>{
+      const {sistemaEleitoral,eleicao,signers} = await loadFixture(deploySistemaEleitoralComEleicaoFixture)
+      await sistemaEleitoral.anexarEleicao(await eleicao.getAnoDeEleicao(),eleicao.target)
+      await sistemaEleitoral.iniciarEleicao(await eleicao.getAnoDeEleicao())
+      await time.increase(60*60*24*30)
+      await expect(sistemaEleitoral.connect(signers[1]).encerrarEleicao(await eleicao.getAnoDeEleicao())).to.be.revertedWithCustomError(sistemaEleitoral,"OwnableUnauthorizedAccount")
+    })
+    it("Não deve encerrar uma eleição que não existe",async()=>{
+      const {sistemaEleitoral} = await loadFixture(deploySistemaEleitoralComEleicaoFixture)
+      await expect(sistemaEleitoral.encerrarEleicao(2024)).to.be.revertedWithCustomError(sistemaEleitoral,"SistemaEleitoral__EleicaoNaoExiste")
     })
 })
