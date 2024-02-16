1d305a0d967a4c97802969cb8317937c51bb09fa
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-02-15 23:49:15 -0300

    Encerrar eleicoes funcionando

--- server/src/web3-services/deployNewEleicao.ts
+++ server/src/web3-services/deployNewEleicao.ts
@@ -3,12 +3,13 @@ import {bytecode} from "./eleicao-bytecode.json"
 import { Eleicao__factory } from "../typechain-types"
 import { adminWallet } from "./config"
 import { Candidato } from "../models/Candidato"
+import { environment } from "../config/environment"
 
 
 export async function deployNewEleicao(anoDaEleicao:string,candidatosIniciais:Candidato[]){
     const eleicaoFactory = new ethers.ContractFactory(Eleicao__factory.abi,bytecode,adminWallet)
     console.log(candidatosIniciais)
-    const eleicao = await eleicaoFactory.deploy(anoDaEleicao,adminWallet.address,candidatosIniciais)
+    const eleicao = await eleicaoFactory.deploy(anoDaEleicao,environment.SISTEMA_ELEITORAL_CONTRACT_ADDRESS,candidatosIniciais)
     await eleicao.waitForDeployment()
     return Eleicao__factory.connect(await eleicao.getAddress(),adminWallet)
     
