069adf28c787130eba0fdae3dc513aa288d71815
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-02-21 15:40:57 -0300

    terminar a versao mobile

--- admin-client/src/App.tsx
+++ admin-client/src/App.tsx
@@ -1,13 +1,17 @@
+import { ThemeProvider, useTheme } from "@emotion/react"
 import { Outlet } from "react-router-dom"
 
 
 
 
 function App() {
-  
+    const theme =useTheme()
   return (
     <>
+    <ThemeProvider theme={theme}>
+
      <Outlet />
+    </ThemeProvider>
     </>
   )
 }
---
+++ admin-client/src/components/DashboardMobile/index.tsx
@@ -0,0 +1,39 @@
+import { AppBar, Box, Tab, Tabs } from "@mui/material";
+import { useState } from "react";
+
+export  function DashboardMobile() {
+    const [value, setValue] = useState(0);
+  return (
+    <Box sx={{ bgcolor: 'background.paper', width: 500 }}>
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="secondary"
+          textColor="inherit"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
+          Item Three
+        </TabPanel>
+      </SwipeableViews>
+    </Box>
+  )
+}
