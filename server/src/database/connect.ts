79c53c2cfd8352e65e1722811d6a2f9378c94c47
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-04-15 14:55:40 -0300

    update connection config

--- server/src/database/connect.ts
+++ server/src/database/connect.ts
@@ -1,10 +1,17 @@
-import {MongoClient,Db} from "mongodb";
+import {MongoClient,Db, ServerApiVersion} from "mongodb";
 import { environment } from "../config/environment";
 
 let singleton:Db
 export default async function connectDB(){
     if(singleton) return singleton;
-    const client = new MongoClient(environment.MONGO_URI);
+    const client = new MongoClient(environment.MONGO_URI,{
+        serverApi:{
+
+            version:ServerApiVersion.v1,
+            strict:true,
+            deprecationErrors:true
+        }
+    });
     await client.connect();
     singleton = client.db("sistema-eleitoral");
     console.log("Connected to database");
