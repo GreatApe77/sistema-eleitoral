dd15160252119dbde5d058b445b328a745298f74
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-05-04 23:17:58 -0300

    fix: update path

--- server/src/middlewares/validate-votar/RequestSchema.ts
+++ server/src/middlewares/validate-votar/RequestSchema.ts
@@ -1,5 +1,5 @@
 import {z} from "zod";
-import { VotarDTO } from "../../use-cases/votar/VotarDTO";
+
 import { isAddress } from "ethers";
 //0xa775955d6e3c8d5fd170db89635c2178f819643a8225b9fa479a510c59a3e6023ae19a6a959f218898bb61e9c82da75775c62f8f9dc96e54a0590ef73b9722c51c
 export const RequestSchema = z.object({
